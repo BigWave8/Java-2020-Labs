8dd153310fe43c4be5da5e8748958d61f6b5397f
tiforn <tarasfqx@gmail.com>
2020-02-19 16:24:54 +0200

    added lab 3 to git hub

---
+++ src/main/java/ua/lviv/iot/lab_3/manager/ShopManager.java
@@ -0,0 +1,16 @@
+package ua.lviv.iot.lab_3.manager;
+
+import java.util.List;
+
+import ua.lviv.iot.lab_3.models.AbstractStationery;
+import ua.lviv.iot.lab_3.models.SortType;
+
+public class ShopManager {
+	public static void sortPensByPriceInHryvnia(List<AbstractStationery> stationeries, SortType sortType) {
+
+	}
+
+	public static void sortPensByTargetAge(List<AbstractStationery> stationeries, SortType sortType) {
+
+	}
+}
---
+++ src/main/java/ua/lviv/iot/lab_3/models/Notebook.java
@@ -0,0 +1,25 @@
+package ua.lviv.iot.lab_3.models;
+
+public class Notebook extends AbstractPaperProducts {
+private boolean oneColorOnCover;
+private Ruling ruling;
+public Notebook(double priceInHryvnia, String producer, String barCode, int targetAge, Format format, int article,
+		int densityInGramsPerSguareMeter, boolean oneColorOnCover, Ruling ruling) {
+	super(priceInHryvnia, producer, barCode, targetAge, format, article, densityInGramsPerSguareMeter);
+	this.oneColorOnCover = oneColorOnCover;
+	this.ruling = ruling;
+}
+public boolean isOneColorOnCover() {
+	return oneColorOnCover;
+}
+public void setOneColorOnCover(boolean oneColorOnCover) {
+	this.oneColorOnCover = oneColorOnCover;
+}
+public Ruling getRuling() {
+	return ruling;
+}
+public void setRuling(Ruling ruling) {
+	this.ruling = ruling;
+}
+
+}
