9bed1ac2d764fbba46eb0afb2a1f6e878aada7e2
BigWave8 <rostykchess@gmail.com>
2020-03-02 23:05:56 +0200

    add sorts methods and test for them

--- src/main/java/ua/lviv/iot/lab_3/manager/ShopManager.java
+++ src/main/java/ua/lviv/iot/lab_3/manager/ShopManager.java
@@ -1,16 +1,60 @@
 package ua.lviv.iot.lab_3.manager;
 
+import java.util.Comparator;
 import java.util.List;
 
 import ua.lviv.iot.lab_3.models.AbstractStationery;
 import ua.lviv.iot.lab_3.models.SortType;
 
 public class ShopManager {
-	public static void sortPensByPriceInHryvnia(List<AbstractStationery> stationeries, SortType sortType) {
+
+	private static final SortByPrice SORT_BY_PRICE = new SortByPrice();
+
+	public static void sortByPrice(List<AbstractStationery> stationeries, SortType sortType) {
+
+		stationeries.sort(sortType == SortType.ASCENDING ? SORT_BY_PRICE : SORT_BY_PRICE.reversed());
+	}
+
+	static class SortByPrice implements Comparator<AbstractStationery> {
+
+		@Override
+		public int compare(AbstractStationery firstStationery, AbstractStationery secondStationery) {
+			return (int) (firstStationery.getPriceInHryvnia() - secondStationery.getPriceInHryvnia());
+		}
 
 	}
 
-	public static void sortPensByTargetAge(List<AbstractStationery> stationeries, SortType sortType) {
+	public static void sortByProducer(List<AbstractStationery> stationeries, SortType sortType) {
+		Comparator<AbstractStationery> comparator = new Comparator<AbstractStationery>() {
+
+			@Override
+			public int compare(AbstractStationery firstStationery, AbstractStationery secondStationery) {
+				return firstStationery.getProducer().compareTo(secondStationery.getProducer());
+			}
+		};
+		stationeries.sort(sortType == SortType.ASCENDING ? comparator : comparator.reversed());
 
 	}
+
+	public static void sortByBarCode(List<AbstractStationery> stationeries, SortType sortType,
+			SortByBarCodeComparator barCodeComparator) {
+
+		stationeries.sort(sortType == SortType.ASCENDING ? barCodeComparator : barCodeComparator.reversed());
+	}
+
+	public class SortByBarCodeComparator implements Comparator<AbstractStationery> {
+
+		@Override
+		public int compare(AbstractStationery firstStationery, AbstractStationery secondStationery) {
+			return firstStationery.getBarCode().compareTo(secondStationery.getBarCode());
+		}
+
+	}
+
+	public static void sortByTargetAge(List<AbstractStationery> stationeries, SortType sortType) {
+		Comparator<AbstractStationery> targetAgeComparator = (firstStationery,
+				secondStationery) -> firstStationery.getTargetAge() - secondStationery.getTargetAge();
+		stationeries.sort(sortType == SortType.ASCENDING ? targetAgeComparator : targetAgeComparator.reversed());
+	}
+
 }
--- src/test/java/ua/lviv/iot/lab_3/ShopManagerTest.java
+++ src/test/java/ua/lviv/iot/lab_3/ShopManagerTest.java
@@ -10,8 +10,8 @@ import ua.lviv.iot.lab_3.models.SortType;
 class ShopManagerTest extends BaseStationeryMakerTest {
 
 	@Test
-	public void testSortGoodsByPrice() {
-		ShopManager.sortPensByPriceInHryvnia(stationeries, SortType.ASCENDING);
+	public void testSortByPrice() {
+		ShopManager.sortByPrice(stationeries, SortType.ASCENDING);
 		assertEquals(7, stationeries.get(0).getPriceInHryvnia());
 		assertEquals(10, stationeries.get(1).getPriceInHryvnia());
 		assertEquals(14, stationeries.get(2).getPriceInHryvnia());
@@ -19,10 +19,32 @@ class ShopManagerTest extends BaseStationeryMakerTest {
 	}
 
 	@Test
-	public void testSortGoodsBySize() {
-		ShopManager.sortPensByTargetAge(stationeries, SortType.ASCENDING);
+	public void testSortByProducer() {
+		ShopManager.sortByProducer(stationeries, SortType.ASCENDING);
+		assertEquals("Gelsone", stationeries.get(0).getProducer());
+		assertEquals("RADIUS", stationeries.get(1).getProducer());
+		assertEquals("RADIUS", stationeries.get(2).getProducer());
+		assertEquals("Sigma", stationeries.get(3).getProducer());
+	}
+	
+	ShopManager shopManager = new ShopManager();
+	ShopManager.SortByBarCodeComparator barCodeComparator = shopManager.new SortByBarCodeComparator();
+	
+	
+	@Test
+	public void testSortByBarCode() {
+		ShopManager.sortByBarCode(stationeries, SortType.ASCENDING, barCodeComparator);
+		assertEquals("2384491", stationeries.get(0).getBarCode());
+		assertEquals("2384492", stationeries.get(1).getBarCode());
+		assertEquals("2384493", stationeries.get(2).getBarCode());
+		assertEquals("2384494", stationeries.get(3).getBarCode());
+	}
+	
+	@Test
+	public void testSortByTargetAge() {
+		ShopManager.sortByTargetAge(stationeries, SortType.ASCENDING);
 		assertEquals(7, stationeries.get(0).getTargetAge());
-		assertEquals(10, stationeries.get(1).getTargetAge());
+		assertEquals(9, stationeries.get(1).getTargetAge());
 		assertEquals(15, stationeries.get(2).getTargetAge());
 		assertEquals(18, stationeries.get(3).getTargetAge());
 	}
